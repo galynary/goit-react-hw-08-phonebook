d9978a7a8abee4b10a8a11c402382de902f37de0
galynary <107271972+galynary@users.noreply.github.com>
2023-05-09 22:41:40 +0300

    change

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { App } from 'components/App';
 import { store, persistor } from 'redux/store';
-import './index.css';
+import 'styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
